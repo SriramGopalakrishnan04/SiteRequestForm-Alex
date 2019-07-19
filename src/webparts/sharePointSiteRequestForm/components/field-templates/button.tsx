a438eccf4e46ff165da7d81581bcc362a0b61c20
Alex Lindemann <AlexDeadmanLindemann@gmail.com>
2019-07-19 12:47:49 -0500

    Updating react

--- src/webparts/sharePointSiteRequestForm/components/field-templates/button.tsx
+++ src/webparts/sharePointSiteRequestForm/components/field-templates/button.tsx
@@ -1,17 +1,71 @@
 import * as React from 'react';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+    },
+    dense: {
+      marginTop: theme.spacing(2),
+    },
+    menu: {
+      width: 200,
+    },
+  }),
+);
 
-export default function CheckboxTemplate() {
-  // const handleChange = (name: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   setState({ ...state, [name]: event.target.checked });
-  // };
+interface State {
+  name: string;
+  age: string;
+  multiline: string;
+  currency: string;
+}
+
+export default function OutlinedTextFields() {
+  const classes = useStyles({});
+  const [values, setValues] = React.useState<State>({
+    name: 'Cat in the Hat',
+    age: '',
+    multiline: 'Controlled',
+    currency: 'EUR',
+  });
+
+  const handleChange = (name: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValues({ ...values, [name]: event.target.value });
+  };
 
   return (
-    <div>
-      <button className="foo-button mdc-button">
-          Button
-      </button>
-    </div>
-    
+    <form className={classes.container} noValidate autoComplete="off">
+      
+      <TextField
+        required
+        id="outlined-required"
+        label="Required"
+        defaultValue="Hello World"
+        className={classes.textField}
+        margin="normal"
+        variant="outlined"
+      />
+      <TextField
+        id="outlined-full-width"
+        label="Label"
+        style={{ margin: 8 }}
+        placeholder="Placeholder"
+        helperText="Full width!"
+        fullWidth
+        margin="normal"
+        variant="outlined"
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+    </form>
   );
 }
